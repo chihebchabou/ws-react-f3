22da7a97de5a7b072a3409ac577cc3b280aae91f
chihebchabou <chihebchaboutech@gmail.com>
2023-01-20 12:04:09 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+// Add these lines for bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
